0d6ccf80a92d5fdba7b51400da443e0b70969e7a
Teo <144486916+TeooL@users.noreply.github.com>
2024-12-04 14:48:55 -0500

    Add files via upload

--- Day4_2024.java
+++ Day4_2024.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Executable;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -21,16 +22,22 @@ public class Day4_2024 {
         }
 
         int num = 0;
+
         for (int i = 0; i < grid.length - 3;i++){
             for (int c = 0;c < grid[i].length - 3;c++){
-                if ((grid[i][c] + grid[i][c + 1] + grid[i][c + 2] + grid[i][c + 3]).equals("XMAS") || (grid[i][c] + grid[i][c + 1] + grid[i][c + 2] + grid[i][c + 3]).equals("SAMX")){
-                    num++;
+                try {
+                    if ((grid[i][c] + grid[i][c + 1] + grid[i][c + 2] + grid[i][c + 3]).equals("XMAS") || (grid[i][c] + grid[i][c + 1] + grid[i][c + 2] + grid[i][c + 3]).equals("SAMX")) {
+                        num++;
+                    }
+                    if ((grid[i][c] + grid[i + 1][c] + grid[i + 2][c] + grid[i + 3][c]).equals("XMAS") || (grid[i][c] + grid[i + 1][c] + grid[i + 2][c] + grid[i + 3][c]).equals("SAMX")) {
+                        num++;
+                    }
+                    if ((grid[i][c] + grid[i + 1][c + 1] + grid[i + 2][c + 2] + grid[i + 3][c + 3]).equals("XMAS") || (grid[i][c] + grid[i + 1][c + 1] + grid[i + 2][c + 2] + grid[i + 3][c + 3]).equals("SAMX")) {
+                        num++;
+                    }
                 }
-                if ((grid[i][c] + grid[i+1][c] + grid[i+2][c] + grid[i+3][c]).equals("XMAS") || (grid[i][c] + grid[i+1][c] + grid[i+2][c] + grid[i+3][c]).equals("SAMX")){
-                    num++;
-                }
-                if ((grid[i][c] + grid[i + 1][c + 1] + grid[i + 2][c + 2] + grid[i + 3][c + 3]).equals("XMAS") || (grid[i][c] + grid[i + 1][c + 1] + grid[i + 2][c + 2] + grid[i + 3][c + 3]).equals("SAMX")) {
-                    num++;
+                catch (Exception e){
+
                 }
             }
         }
