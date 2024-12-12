b2a1d4208942ce44202240f0737099486c59c4ef
Teo <144486916+TeooL@users.noreply.github.com>
2024-12-12 14:50:24 -0500

    Add files via upload

--- Day2_2024.java
+++ Day2_2024.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -49,45 +50,34 @@ public class Day2_2024 {
 
         int dampener = 0;
         for (int i = 0; i < arr.size();i++){
-            int count = 0;
-            int wrongLevel = 0;
-            for (int n = 0; n < arr.get(i).size()-1;n ++) {
-                    if (arr.get(i).get(n) > arr.get(i).get(n + 1)) {
-                        if (arr.get(i).get(n) - arr.get(i).get(n + 1) <= 3) {
-                            count++;
-                        } else {
-                            wrongLevel++;
-                        }
-                    }
-                    else {
-                        break;
-                    }
+            ArrayList<Integer> currentLevel = arr.get(i);
+            boolean allIncreasingSafe = true;
+            boolean allDecreasing = true;
+
+            for (int e = 0; e< currentLevel.size() - 1;e++){ //All Increasing
+                if (currentLevel.get(e+1) <= currentLevel.get(e)){
+                    allIncreasingSafe = false;
+                    break;
                 }
-            if (wrongLevel == 1){
+                else if (currentLevel.get(e+1) - currentLevel.get(e) > 3){
+                    allIncreasingSafe = false;
+                    break;
+                }
+            }
+            if (allIncreasingSafe){
                 dampener++;
             }
-        }
-        for (int i = 0; i < arr.size();i++){
-            int count = 0;
-            int wrongLevel = 0;
-            for (int n = 0; n < arr.get(i).size()-1;n ++) {
-                    if (arr.get(i).get(n) < arr.get(i).get(n + 1)) {
-                        if (arr.get(i).get(n + 1) - arr.get(i).get(n) <= 3) {
-                            count++;
-                        }
-                        else{
-                            wrongLevel++;
-                        }
-                    }
-                    else if (arr.get(i).get(n) == arr.get(i).get(n + 1)) {
-                        wrongLevel++;
-                    }
-                    else {
-                        break;
-                    }
+
+            for (int e = 0; e< currentLevel.size() - 1;e++){
+                if (currentLevel.get(e+1) >= currentLevel.get(e)){
+                    allDecreasing = false;
+                    break;
                 }
-            if (wrongLevel == 1){
-                dampener++;
+            }
+
+
+            for (int n = 0; n < currentLevel.size() - 1;n++){
+
             }
         }
         System.out.println(dampener);
