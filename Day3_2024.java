93abf760e04d9396e8b6da815a0e7b7eff1a4722
Teo <144486916+TeooL@users.noreply.github.com>
2024-12-12 14:33:19 -0500

    Add files via upload

--- Day3_2024.java
+++ Day3_2024.java
@@ -1,3 +1,4 @@
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -30,5 +31,39 @@ public class Day3_2024 {
         }
         System.out.println(allMatches);
         System.out.println(total);
+
+        //Part 2
+        File f2 = new File("src/day3input.txt");
+        Scanner s2 = new Scanner(f2);
+        ArrayList<String> newList = new ArrayList<>();
+        boolean enabled = true;
+        while (s2.hasNext()){
+            String l = s2.nextLine();
+            String regex2 = "mul\\([1-9][0-9]{0,2},[1-9][0-9]{0,2}\\)|do\\(\\)|don't\\(\\)";
+            Matcher m2 = Pattern.compile(regex2).matcher(l);
+            while (m2.find()){
+                newList.add(m2.group());
+            }
+        }
+        int part2total = 0;
+        System.out.println(newList);
+        for (String i: newList){
+            if (i.equals("don't()")){
+                enabled = false;
+            }
+            else if (i.equals("do()")){
+                enabled = true;
+            }
+            else if (i.contains("mul") && enabled){
+                int num1Start = i.indexOf("(");
+                int num1End = i.indexOf(",");
+                int num2End = i.indexOf(")");
+                int firstNum = Integer.parseInt(i.substring(num1Start + 1, num1End));
+                int secNum = Integer.parseInt(i.substring(num1End + 1, num2End));
+                part2total += firstNum * secNum;
+            }
+        }
+        System.out.println(part2total);
     }
 }
+
