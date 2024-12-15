90e1ec45631c0378eab5e41858846193c37d3981
Teo <144486916+TeooL@users.noreply.github.com>
2024-12-15 12:33:35 -0500

    Add files via upload

--- Day6_2024.java
+++ Day6_2024.java
@@ -12,7 +12,7 @@ public class Day6_2024 {
             allMatches.add(s.nextLine());
         }
         int rows = allMatches.size();
-        int columns = allMatches.getFirst().length();
+        int columns = allMatches.get(0).length();
         String[][] grid = new String[rows][columns];
         int startRow = 0;
         int startColumn = 0;
@@ -25,45 +25,74 @@ public class Day6_2024 {
                 }
             }
         }
-        boolean notRepeating = true;
+
+        // Part One
+        int posCount = 0;
         String direction = "up";
-        ArrayList<String> positions = new ArrayList<>();
-        while (notRepeating){
-            positions.add(startRow + "|" + startColumn);
+        String[][] path = new String[grid.length][grid[0].length];
+        for (int r = 0; r < path.length;r++){
+            for (int c = 0; c < path[0].length;c++){
+                path[r][c] = ".";
+            }
+        }
+        path[startRow][startColumn] = "X";
+        while (true){
             if (direction.equals("up")){
+                if (startRow - 1 < 0){
+                    break;
+                }
                 if (grid[startRow - 1][startColumn].equals("#")){
                     direction = "right";
                 }
                 else{
                     startRow--;
+                    path[startRow][startColumn] = "X";
                 }
             }
             if (direction.equals("right")){
+                if (startColumn + 1 == grid[0].length){
+                    break;
+                }
                 if (grid[startRow][startColumn+1].equals("#")){
                     direction = "down";
                 }
                 else{
                     startColumn++;
+                    path[startRow][startColumn] = "X";
                 }
             }
             if (direction.equals("down")){
+                if (startRow + 1 == grid.length){
+                    break;
+                }
                 if (grid[startRow+1][startColumn].equals("#")){
                     direction = "left";
                 }
                 else{
                     startRow++;
+                    path[startRow][startColumn] = "X";
                 }
             }
             if (direction.equals("left")){
+                if (startColumn - 1 < 0){
+                    break;
+                }
                 if (grid[startRow][startColumn-1].equals("#")){
                     direction = "up";
                 }
                 else{
                     startColumn--;
+                    path[startRow][startColumn] = "X";
                 }
             }
-            for (int i = 0; i<positions.size();i++){
+        }
+        for (int i = 0; i < path.length; i++){
+            for (int n = 0; n < path[0].length;n++){
+                if (path[i][n].equals("X")){
+                    posCount++;
+                }
             }
         }
+        System.out.println(posCount);
     }
 }
