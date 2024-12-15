6dd2cd75a39c4c8ccc2318c37f4b61b0fa87cdd3
Teo <144486916+TeooL@users.noreply.github.com>
2024-12-15 14:48:32 -0500

    Add files via upload

--- Day2_2024.java
+++ Day2_2024.java
@@ -1,4 +1,5 @@
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -48,38 +49,94 @@ public class Day2_2024 {
         System.out.println(safe);
 
 
-        int dampener = 0;
+        int safe2 = 0;
         for (int i = 0; i < arr.size();i++){
-            ArrayList<Integer> currentLevel = arr.get(i);
+            ArrayList<Integer> level = arr.get(i);
+            // Check if safe
+            //First all increasing and differ by 1 <= x <= 3
             boolean allIncreasingSafe = true;
-            boolean allDecreasing = true;
-
-            for (int e = 0; e< currentLevel.size() - 1;e++){ //All Increasing
-                if (currentLevel.get(e+1) <= currentLevel.get(e)){
-                    allIncreasingSafe = false;
-                    break;
-                }
-                else if (currentLevel.get(e+1) - currentLevel.get(e) > 3){
-                    allIncreasingSafe = false;
-                    break;
+            for (int n = 0; n < level.size() - 1 ; n++){
+                if (level.get(n+1) < level.get(n) || level.get(n+1) - level.get(n) > 3 || level.get(n+1) - level.get(n) < 1){
+                 allIncreasingSafe = false;
+                 break;
                 }
             }
             if (allIncreasingSafe){
-                dampener++;
+                System.out.println(level);
+                safe2++;
             }
-
-            for (int e = 0; e< currentLevel.size() - 1;e++){
-                if (currentLevel.get(e+1) >= currentLevel.get(e)){
-                    allDecreasing = false;
+            //Second all decreasing and differ by 1 <= x <= 3
+            boolean allDecreasingSafe = true;
+            for (int n = 0; n < level.size() - 1; n++){
+                if (level.get(n) < level.get(n+1) || level.get(n) - level.get(n+1) > 3 || level.get(n) - level.get(n+1) < 1){
+                    allDecreasingSafe = false;
                     break;
                 }
             }
+            if (allDecreasingSafe){
+                System.out.println(level);
+                safe2++;
+            }
 
+            //Third check unsafe levels for single bad level
+            boolean mostlyIncreasing = false;
+            boolean mostlyDecreasing = false;
+            int incrCount = 0;
+            int decrCount = 0;
+            for (int n = 0; n < level.size()-1;n++){
+                if (level.get(n+1) - level.get(n) > 0){
+                    incrCount++;
+                }
+                if (level.get(n+1) - level.get(n) < 0){
+                    decrCount++;
+                }
+            }
+            if (incrCount > decrCount){
+                mostlyIncreasing = true;
+            } else{
+                mostlyDecreasing = true;
+            }
 
-            for (int n = 0; n < currentLevel.size() - 1;n++){
-
+            ArrayList<Integer> currentLevel = level;
+            if (mostlyIncreasing){
+                int removes = 0;
+                for (int n = 0; n < currentLevel.size() - 1;n++){
+                    if (currentLevel.get(n+1) - currentLevel.get(n) > 3 || currentLevel.get(n) - currentLevel.get(n+1) == 0){
+                        currentLevel.remove(n);
+                        removes++;
+                        n=0;
+                    }
+                    else if (currentLevel.get(n) > currentLevel.get(n+1)){
+                        currentLevel.remove(n+1);
+                        removes++;
+                        n=0;
+                    }
+                }
+                if (removes == 1){
+                    System.out.println(currentLevel);
+                    safe2++;
+                }
+            }
+            if (mostlyDecreasing){
+                int removes = 0;
+                for (int n = 0; n < currentLevel.size() - 1; n++){
+                    if (currentLevel.get(n) - currentLevel.get(n+1) > 3 || currentLevel.get(n) - currentLevel.get(n+1) == 0){
+                        currentLevel.remove(n);
+                        removes++;
+                        n=0;
+                    }
+                    else if (currentLevel.get(n) < currentLevel.get(n+1)){
+                        currentLevel.remove(n+1);
+                        removes++;
+                        n=0;
+                    }
+                }
+                if (removes == 1){
+                    System.out.println(currentLevel);
+                    safe2++;
+                }
             }
         }
-        System.out.println(dampener);
+        System.out.println(safe2);
     }
 }
